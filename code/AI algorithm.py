f6250ccff458426394c715283f41cf0c5af95d89
shepai <dextershepherd@hotmail.co.uk>
2019-08-13 16:40:08 +0100

    Add files via upload

--- code/AI algorithm.py
+++ code/AI algorithm.py
@@ -57,7 +57,7 @@ def update():
               file = open(sys.path[0]+"/"+"temp.txt","r")
               r = file.read()
               file.close()
-              current = open(sys.path[0]+"/"+"AI\ algorithm.py","r")
+              current = open(sys.path[0]+"/"+"AI algorithm.py","r")
               r2 = current.read()
               current.close()
               if(r == r2):#same
@@ -65,7 +65,7 @@ def update():
               else:
                      #update
                      OUTPUT("updating...")
-                     current = open(sys.path[0]+"/"+"AI\ algorithm.py","w")
+                     current = open(sys.path[0]+"/"+"AI algorithm.py","w")
                      current.write(r)
                      current.close()
                      os.system("sudo reboot")    #restart with new
