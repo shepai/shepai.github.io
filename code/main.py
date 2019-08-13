55ce97b6e560128a8ad6fab1fdbc4690ee4eb70b
shepai <dextershepherd@hotmail.co.uk>
2019-08-13 14:54:15 +0100

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -37,13 +37,13 @@ def update():
 
 update()
 try:
-    file=open(sys.path[0]+"/"+"V 0.0.7"+"/AI.py",'r')
+    file=open(sys.path[0]+"/"+"V0.0.7"+"/AI.py",'r')
     file.close()
     print("file found")
-    os.system('python3 '+sys.path[0]+'/'+'V\ 0.0.7'+'/AI.py')
+    os.system('python3 '+sys.path[0]+'/'+'V0.0.7'+'/AI.py')
 except:
     print("download")
-    r = requests.get("http://shepai.github.io/code/V%200.0.7.zip")
+    r = requests.get("http://shepai.github.io/code/V0.0.7.zip")
     z = zipfile.ZipFile(io.BytesIO(r.content))
     z.extractall()
 
@@ -51,6 +51,6 @@ except:
     r=file.read()
     file.close()
     
-    r+="\nsudo python3 "+sys.path[0]+"/"+"V 0.0.7"
+    r+="\nsudo python3 "+sys.path[0]+"/"+"V0.0.7"
     print(r)
-    os.system('python3 '+sys.path[0]+'/'+'V\ 0.0.7'+'/AI.py')
+    os.system('python3 '+sys.path[0]+'/'+'V0.0.7'+'/AI.py')
