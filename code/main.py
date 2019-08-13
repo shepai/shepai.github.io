770099f09529508dbee13b72e73cdbeef73986aa
shepai <dextershepherd@hotmail.co.uk>
2019-08-13 15:02:04 +0100

    Add files via upload
    file slash

--- code/main.py
+++ code/main.py
@@ -9,7 +9,7 @@ except:
 from urllib.request import urlopen
 def update():
        try:
-              file = open(sys.path[0]+"temp.txt","w")
+              file = open(sys.path[0]+"/temp.txt","w")
               for line in urlopen("https://shepai.github.io/code/main.py"):
                      #decode the file and write it to the Pi
                      s = line.decode('utf-8')
@@ -17,10 +17,10 @@ def update():
                      file.write(s)
               
               file.close()
-              file = open(sys.path[0]+"temp.txt","r")
+              file = open(sys.path[0]+"/temp.txt","r")
               r = file.read()
               file.close()
-              current = open(sys.path[0]+"main.py","r")
+              current = open(sys.path[0]+"/main.py","r")
               r2 = current.read()
               current.close()
               if(r == r2):#same
@@ -28,7 +28,7 @@ def update():
               else:
                      #update
                      print("updating...")
-                     current = open(sys.path[0]+"main.py","w")
+                     current = open(sys.path[0]+"/main.py","w")
                      current.write(r)
                      current.close()
                      os.system("sudo reboot")    #restart with new
