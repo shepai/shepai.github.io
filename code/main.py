dae17c5e08f50f5fd1f8f0982b77936bd9f43b8b
shepai <dextershepherd@hotmail.co.uk>
2019-08-13 14:46:54 +0100

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -7,24 +7,6 @@ try:
 except:
        import http.client as httplib
 from urllib.request import urlopen
-update()
-try:
-    file=open(sys.path[0]+"/"+"V 0.0.7"+"/AI.py",'r')
-    file.close()
-    print("file found")
-    os.system('python3 '+sys.path[0]+'/'+'V\ 0.0.7'+'/AI.py')
-except:
-    print("download")
-    r = requests.get("http://shepai.github.io/code/V%200.0.7.zip")
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall()
-
-    file = open("/etc/profile","r")
-    r=file.read()
-    file.close()
-
-    r+="\nsudo python3 "+sys.path[0]+"/"+"V 0.0.7"
-    print(r)
 def update():
        try:
               file = open(sys.path[0]+"temp.txt","w")
@@ -52,3 +34,22 @@ def update():
                      os.system("sudo reboot")    #restart with new
        except:
               print("Error finding update")
+
+update()
+try:
+    file=open(sys.path[0]+"/"+"V 0.0.7"+"/AI.py",'r')
+    file.close()
+    print("file found")
+    os.system('python3 '+sys.path[0]+'/'+'V\ 0.0.7'+'/AI.py')
+except:
+    print("download")
+    r = requests.get("http://shepai.github.io/code/V%200.0.7.zip")
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall()
+
+    file = open("/etc/profile","r")
+    r=file.read()
+    file.close()
+
+    r+="\nsudo python3 "+sys.path[0]+"/"+"V 0.0.7"
+    print(r)
