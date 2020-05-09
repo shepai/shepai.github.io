96badc83b16ae48f870bf7603df3ddeb5fac9fe3
shepai <dextershepherd@hotmail.co.uk>
2020-05-09 12:22:48 +0100

    Add files via upload
    nnnnnn

--- code/cleverbot1.py
+++ code/cleverbot1.py
@@ -2,27 +2,30 @@ import cleverbotfree.cbfree
 import sys
 from selenium import webdriver
 
-cb = cleverbotfree.cbfree.Cleverbot()
 
-def chat(userInput):
-    response = cb.single_exchange(userInput)
-    return response
+class bot:
+    def __init__(b):
+        b.cb = cleverbotfree.cbfree.Cleverbot()
+
+    def chat(b,userInput):
+        response = b.cb.single_exchange(userInput)
+        return response
 slashn="""
 """
 
-userInput = input('User: ')
-file=open("convo.txt","a") #create file
-file.write("bot1: "+userInput+slashn)
-file.close()
+bot1=bot()
+bot2=bot()
+
+response2=input(">")
 
 while True:
-    file=open("convo.txt","a")
-    previous=chat(userInput)
-    print(previous)
-    file.write("bot2: "+previous+slashn)
-    userInput=chat(previous)
-    file.write("bot1: "+userInput+slashn)
-    print(userInput)
+    file=open("convo1.txt","a")
+    response1=bot1.chat(response2)
+    file.write("bot1: "+response1+slashn)
+    response2=bot2.chat(response1)
+    file.write("bot2: "+response2+slashn)
     file.close()
+    print("bot1:",response1)
+    print("bot2",response2)
 cb.browser.close()
 sys.exit()
